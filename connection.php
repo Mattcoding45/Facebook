bbecc75fb385d836835a815cb271fd07a211ec1e
Matthias <matthias.munsch@coda-student.school>
2023-11-23 10:38:46 +0100

    login & connection

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php 
+
+session_start();
+
+if (!isset($_SESSION['csrf_token'])) {
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+}
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=facebook;charset=utf8','root');
+    $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
